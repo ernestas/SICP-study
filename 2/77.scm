ce0f94bd81b03cfc748fa2ad3b8f31f02794e256
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2011-05-21 18:49:46 +0100

    2.5.1

---
+++ 2/77.scm
@@ -0,0 +1,166 @@
+;; *******************************
+;; No need to write all this down.
+;; Explanation at the bottom.
+
+
+
+;; ******
+;; square
+
+(define (square x) (* x x))
+
+;; ********************
+;; apply-generic & tags
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these types -- APPLY-GENERIC" (list op type-tags))))))
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum -- TYPE-TAG" datum)))
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum -- CONTENTS" datum)))
+
+
+
+;; ****************************
+;; rectangular & polar packages
+
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z) (sqrt (+ (square (real-part z))
+                                 (square (imag-part z)))))
+  (define (angle z) (atan (imag-part z)
+                          (real-part z)))
+  (define (make-from-mag-ang r a) (cons (* r (cos a))
+                                        (* r (sin a))))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z) (* (magnitude z) 
+                           (cos (angle z))))
+  (define (imag-part z) (* (magnitude z)
+                           (sin (angle z))))
+  (define (make-from-real-imag x y) (cons (sqrt (+ (square x) (square y)))
+                                          (atan y x)))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+
+
+;; ***************
+;; complex package
+
+(define (install-complex-package)
+  ;; imported procedures from rectangular and polar packages
+  (install-rectangular-package)
+  (install-polar-package)
+  (define (real-part z) (apply-generic 'real-part z))
+  (define (imag-part z) (apply-generic 'imag-part z))
+  (define (magnitude z) (apply-generic 'magnitude z))
+  (define (angle z) (apply-generic 'angle z))
+  (define (make-from-real-imag x y) ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a) ((get 'make-from-mag-ang 'polar) r a))
+  ;; internal procedures
+  (define (add-complex z1 z2) (make-from-real-imag (+ (real-part z1) (real-part z2))
+                                                   (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2) (make-from-real-imag (- (real-part z1) (real-part z2))
+                                                   (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2) (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                                                 (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2) (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                                                 (- (angle z1) (angle z2))))
+  ;; interface to rest of the system
+  (define (tag z) (attach-tag 'complex z))
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+;; *************************
+;; complex exposed to public
+
+(define (make-complex-from-real-imag x y) ((get 'make-from-real-imag 'complex) x y))
+(define (make-complex-from-mag-ang r a) ((get 'make-from-mag-ang 'complex) r a))
+
+(define (magnitude-pub z) ((get 'magnitude '(complex)) z))
+
+
+
+;; *****
+;; tests
+
+;; *****
+;; put & get procedures
+;; NOTE: not yet covered in the book
+;; used solely for the purposes of testing
+(define *op-table* (make-hash-table 'equal))
+(define (put op type proc)
+  (hash-table-put! *op-table* (list op type) proc))
+(define (get op type)
+  (hash-table-get *op-table* (list op type) '()))
+;; *****
+
+(install-complex-package)
+
+(define z (cons 'complex (cons 'rectangular (cons 3 4))))
+z
+(define z (make-complex-from-real-imag 3 4))
+z
+
+(magnitude-pub z)
+
+;; Figure 2.23 in the book explains very well what happens here.
+;;
+;; Two-level data-directed dispatching is in place.
+;; Number is tagged as complex so that complex package is used on it.
+;; Complex package in turn installs rectangular and polar packages, and
+;; depending on inner tag of number z, uses rectangular or polar package on it.
+;;
+;; apply-generic is invoked twice.
+;; First (magnitude-pub z) dispatches to 'magnitude' procedure in complex-package,
+;; which in turn dispatches to 'magnitude' procedure in rectangular-package.
---
+++ 2/78.scm
@@ -0,0 +1,80 @@
+;; ********************
+;; apply-generic & tags
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these types -- APPLY-GENERIC" (list op type-tags))))))
+
+(define (attach-tag type-tag contents)
+  (if (number? contents)
+      contents
+      (cons type-tag contents)))
+
+(define (type-tag datum)
+  (cond ((number? datum) 'scheme-number)
+        ((pair? datum) (car datum))
+        (else (error "Bad tagged datum -- TYPE-TAG" datum))))
+
+(define (contents datum)
+  (cond ((number? datum) datum)
+        ((pair? datum) (cdr datum))
+        (else (error "Bad tagged datum -- CONTENTS" datum))))
+
+;; ******************
+;; generic arithmetic
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+
+
+;; *********************
+;; scheme-number-package
+
+(define (install-scheme-number-package)
+  (define (tag x)
+    (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number
+       (lambda (x) (tag x)))
+  'done)
+
+(define (make-scheme-number n) 
+  ((get 'make 'scheme-number) n))
+
+
+
+;; *****
+;; tests
+
+;; *****
+;; put & get procedures
+;; NOTE: not yet covered in the book
+;; used solely for the purposes of testing
+(define *op-table* (make-hash-table 'equal))
+(define (put op type proc)
+  (hash-table-put! *op-table* (list op type) proc))
+(define (get op type)
+  (hash-table-get *op-table* (list op type) '()))
+;; *****
+
+
+(install-scheme-number-package)
+
+(define a (make-scheme-number 5))
+(define b (make-scheme-number 3))
+
+(add a b)
+(add 3 5)
