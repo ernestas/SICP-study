e3c65c969c806bf61e317ee24683662bf9fdfb96
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2011-06-19 22:51:28 +0100

    add min & max which aren't covered yet

--- 1/03.scm
+++ 1/03.scm
@@ -4,5 +4,19 @@
 (define (sum-of-squares a b)
   (+ (square a) (square b)))
 
+(define (max-of-two a b)
+  (if (< a b)
+      b
+      a))
+
+(define (min-of-two a b)
+  (if (< a b)
+      a
+      b))
+
 (define (sum-of-two-biggest-squares a b c)
-  (sum-of-squares (max a b) (max c (min a b))))
+  (sum-of-squares (max-of-two a b) 
+                  (max-of-two c (min-of-two a b))))
+
+;; test
+(sum-of-two-biggest-squares 1 2 3)
