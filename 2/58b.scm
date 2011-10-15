6812938058622387d00ac887b220aec66e06976f
ernestas <ernestas.lisauskas@gmail.com>
2011-10-15 14:30:54 +0100

    separate deriv procedure

--- 2/58b.scm
+++ 2/58b.scm
@@ -1,30 +1,5 @@
-;; deriv
-
-(define (deriv exp var)
-  (cond ((number? exp) 0)
-        ((variable? exp) (if (same-variable? exp var) 1 0))
-        ((sum? exp) (make-sum (deriv (addend exp) var)
-                              (deriv (augend exp) var)))
-        ((product? exp) (make-sum (make-product (multiplier exp)
-                                                (deriv (multiplicand exp) var))
-                                  (make-product (deriv (multiplier exp) var)
-                                                (multiplicand exp))))
-        ((exponentiation? exp) (make-product (make-product (exponent exp)
-                                                           (make-exponentiation (base exp)
-                                                                                (- (exponent exp) 1)))
-                                             (deriv (base exp) var)))
-        (else (error "unknown expression type -- DERIV" exp))))
-
-;; variable & number
-
-(define (variable? x)
-  (symbol? x))
-
-(define (same-variable? v1 v2)
-  (and (variable? v1) (variable? v2) (eq? v1 v2)))
-
-(define (=number? exp num)
-  (and (number? exp) (= exp num)))
+;; deriv, variable?, same-variable?, =number?
+(include "56_deriv.scm")
 
 ;; list-end
 
