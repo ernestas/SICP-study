56d554a40fe791b4c16c5d48a8c88712aa8677a2
ernestas <ernestas.lisauskas@gmail.com>
2011-10-20 17:04:54 +0100

    separate put & get procedures

--- 2/78.scm
+++ 2/78.scm
@@ -59,16 +59,8 @@
 ;; *****
 ;; tests
 
-;; *****
 ;; put & get procedures
-;; NOTE: not yet covered in the book
-;; used solely for the purposes of testing
-(define *op-table* (make-hash-table 'equal))
-(define (put op type proc)
-  (hash-table-put! *op-table* (list op type) proc))
-(define (get op type)
-  (hash-table-get *op-table* (list op type) '()))
-;; *****
+(include "73_put_get.scm")
 
 
 (install-scheme-number-package)
--- 2/82.scm
+++ 2/82.scm
@@ -202,16 +202,9 @@
   (hash-table-put! *coercion-table* (list type-from type-to) proc))
 (define (get-coercion type-from type-to)
   (hash-table-get *coercion-table* (list type-from type-to) #f))
-;; *****
+
 ;; put & get procedures
-;; NOTE: not yet covered in the book
-;; used solely for the purposes of testing
-(define *op-table* (make-hash-table 'equal))
-(define (put op type proc)
-  (hash-table-put! *op-table* (list op type) proc))
-(define (get op type)
-  (hash-table-get *op-table* (list op type) '()))
-;; *****
+(include "73_put_get.scm")
 
 
 (install-scheme-number-package)
