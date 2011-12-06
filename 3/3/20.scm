c9a30154eed70cadc537bdd09b6dff15705e2c35
ernestas <ernestas.lisauskas@gmail.com>
2011-12-06 17:14:09 +0000

    3.3.1 mutation is just assignment

---
+++ 3/3/20.scm
@@ -0,0 +1,29 @@
+(define (cons x y)
+  (define (set-x! v) (set! x v))
+  (define (set-y! v) (set! y v))
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+          ((eq? m 'cdr) y)
+          ((eq? m 'set-car!) set-x!)
+          ((eq? m 'set-cdr!) set-y!)
+          (else (error "Undefined operation -- CONS" m))))
+  dispatch)
+
+(define (car z) (z 'car))
+(define (cdr z) (z 'cdr))
+
+(define (set-car! z new-value)
+  ((z 'set-car!) new-value)
+  z)
+(define (set-cdr! z new-value)
+  ((z 'set-cdr!) new-value)
+  z)
+
+;; Similar to 3/2/11.scm message passing
+;;
+;; cons is like make-account
+
+(define x (cons 1 2))
+(define z (cons x x))
+(set-car! (cdr z) 17)
+(car x)
