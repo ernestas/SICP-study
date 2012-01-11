7c32f87744f48ef492d5ae08ffa0d2a2c8d35a0f
ernestas <ernestas.lisauskas@gmail.com>
2012-01-11 15:36:19 +0000

    3.4.1 The Nature of Time in Concurrent Systems

---
+++ 3/4/38.scm
@@ -0,0 +1,14 @@
+;; a
+
+;; Peter Paul  Mary  => 45
+;; Paul  Peter Mary  => 45
+;; Peter Mary  Paul  => 35
+;; Paul  Mary  Peter => 50
+;; Mary  Peter Paul  => 40
+;; Mary  Paul  Peter => 40
+
+;; b
+
+;; In between reading the balance and updating it
+;; someone else reads and updates it, thus making
+;; our balance figure wrong.
