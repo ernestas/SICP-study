b2a37d18708fd0ffdcc2b0cd1336c3b00ba2aa61
ernestas <ernestas.lisauskas@gmail.com>
2012-01-16 20:44:33 +0000

    separate prime?

--- 1/2/28.scm
+++ 1/2/28.scm
@@ -1,33 +1,4 @@
-(define (square x) (* x x))
-
-(define (miller-rabin-expmod base exp m)
-  (define (check candidate)
-    (define (check-nontrivial candidate square)
-      (if (and (not (= candidate 1))
-               (not (= candidate (- m 1)))
-               (= square 1))
-          0
-          square))
-    (check-nontrivial candidate (remainder (square candidate) m)))
-  (cond ((= exp 0) 1)
-        ((even? exp) (check (miller-rabin-expmod base (/ exp 2) m)))
-        (else (remainder (* base (miller-rabin-expmod base (- exp 1) m))
-                         m))))
-
-(define (miller-rabin-test n)
-  (define (try-it a)
-    (define (check-it x)
-      (and (not (= x 0)) (= x 1)))
-    (check-it (miller-rabin-expmod a (- n 1) n)))
-  (try-it (+ 1 (random (- n 1)))))
-
-(define (fast-prime? n times)
-  (cond ((= times 0) true)
-        ((miller-rabin-test n) (fast-prime? n (- times 1)))
-        (else false)))
-
-(define (prime? n)
-  (fast-prime? n 100))
+(include "28_prime.scm")
 
 (prime? 2)
 (prime? 3)
