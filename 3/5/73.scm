51632594bb5cf2f25af46abfaae69c86bf2b8b44
ernestas <ernestas.lisauskas@gmail.com>
2012-02-01 19:20:17 +0200

    2.5.3.a Streams as signals

---
+++ 3/5/73.scm
@@ -0,0 +1,17 @@
+;; TODO: review
+
+(define (integral integrand initial-value dt)
+  (define int
+    (cons-stream initial-value
+                 (add-streams (scale-stream integrand dt)
+                              int)))
+  int)
+
+(define (RC R C dt)
+  (lambda (i v0)
+    (add-stream (scale-stream i R)
+                (integral (scale-stream i (/ 1 C))
+                          v0
+                          dt))))
+
+(define RC1 (RC 5 1 0.5))
---
+++ 3/5/74.scm
@@ -0,0 +1,15 @@
+;; TODO: review
+
+(define (make-zero-crossings input-stream last-value)
+  (cons-stream (sign-change-detector (stream-car input-stream)
+                                     last-value)
+               (make-zero-crossings (stream-cdr input-stream)
+                                    (stream-car input-stream))))
+
+(define zero-crossings
+  (make-zero-crossings sense-data 0))
+
+(define zero-crossings
+  (stream-map sign-change-detector
+              sense-data
+              (cons-stream 0 sense-data))
