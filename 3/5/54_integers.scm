b2a54dc19a42600e89046ef973ac39ed1f638533
ernestas <ernestas.lisauskas@gmail.com>
2012-02-05 20:13:59 +0200

    add alternative version of integers

--- 3/5/54_integers.scm
+++ 3/5/54_integers.scm
@@ -2,3 +2,11 @@
 (define (integers-starting-from n)
   (cons-stream n (integers-starting-from (+ n 1))))
 (define integers (integers-starting-from 1))
+
+;; alternative version
+(load "55_add.scm")
+(define ones (cons-stream 1
+                          ones))
+(define integers (cons-stream 1
+                              (add-streams integers
+                                           ones)))
