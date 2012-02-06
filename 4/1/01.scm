bc2e1522503085c84f9074982e7212127d00af62
ernestas <ernestas.lisauskas@gmail.com>
2012-02-06 20:46:22 +0200

    4.1.1 The Core of the Evaluator

---
+++ 4/1/01.scm
@@ -0,0 +1,15 @@
+;; left to right
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((first (eval (first-operand exps) env)))
+        (cons first
+              (list-of-values (rest-operands exps) env)))))
+
+;; right to left
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((rest (list-of-values (rest-operands exps) env)))
+        (cons (eval (first-operand exps) env)
+              rest))))
