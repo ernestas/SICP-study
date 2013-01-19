a21f341dde3ffc5f24cea9a0fc93328344426b4b
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2013-01-19 16:56:14 +0000

    Use redefined eval and add test

--- src/sicp/4/1/01.scm
+++ src/sicp/4/1/01.scm
@@ -1,15 +1,25 @@
+(load "00_evaluator.scm")
+(load "../../../../vendor/test-manager/load.scm")
+(load "00_test.scm")
+
 ;; left to right
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
-      (let ((first (eval (first-operand exps) env)))
+      (let ((first (eval* (first-operand exps) env)))
         (cons first
               (list-of-values (rest-operands exps) env)))))
+
+(define the-global-environment (setup-environment))
+(run-registered-tests)
 
 ;; right to left
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
       (let ((rest (list-of-values (rest-operands exps) env)))
-        (cons (eval (first-operand exps) env)
+        (cons (eval* (first-operand exps) env)
               rest))))
+
+(define the-global-environment (setup-environment))
+(run-registered-tests)
