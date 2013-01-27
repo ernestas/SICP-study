841cbf6c9fadf4146a8df50c542b41cb81a250df
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2013-01-27 08:41:04 +0000

    4.12

---
+++ src/sicp/4/1/12_evaluator.scm
@@ -0,0 +1,42 @@
+;; put & get procedures
+(load "03_put_get.scm")
+
+;; Eval
+(load "03_eval.scm")
+
+(load "00_self_evaluating.scm")
+(define (variable? exp) (symbol? exp))
+(load "03_quoted.scm")
+(load "03_assignment.scm")
+(load "03_definition.scm")
+(load "03_if.scm")
+(load "00_boolean.scm")
+(load "03_lambda.scm")
+(load "03_begin.scm")
+(load "05_cond.scm")
+(load "00_application.scm")
+(load "04_and_or_derived.scm")
+(load "08_let.scm")
+(load "07_let*.scm")
+(load "09_while.scm")
+
+(define (interpret exp)
+  (eval* exp the-global-environment))
+
+;; Apply
+(load "00_apply.scm")
+
+(load "00_primitive_procedure.scm")
+(load "00_compound_procedure.scm")
+
+;; Common stuff
+(load "00_eval_sequence.scm")
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+(load "12_environment.scm")
+
+'METACIRCULAR-EVALUATOR-LOADED
