661a4cbb54f63b4c0e85e89f8ded00dbf53d8f14
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2013-02-09 17:49:15 +0000

    4.25

---
+++ src/sicp/4/2/25.scm
@@ -0,0 +1,3 @@
+;; Evaluating (factorial 5) will enter in an infnite loop since the arguments
+;; are evaluated before the procedure is applied. The definition will work in
+;; normal-order language.
