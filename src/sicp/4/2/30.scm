a30acd387996bb1b7efc94fc554a60b0a6401f63
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2013-09-30 18:31:09 +0100

    4.30

---
+++ src/sicp/4/2/30.scm
@@ -0,0 +1,8 @@
+;a. Ben is right about the behavior of for-each because display is a primitive
+;   function which will force its argument.
+
+;b. (1 2) & 1 and (1 2) & (1 2)
+
+;c. display is still a primitive function.
+
+;d. I like the approach in the text.
