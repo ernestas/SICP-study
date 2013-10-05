07843ea2fe819ca72c35930a5ea0ec9715723051
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2013-10-05 18:31:00 +0100

    4.37-38

---
+++ src/sicp/4/3/38.scm
@@ -0,0 +1,33 @@
+(load "35_evaluator.scm")
+
+(define the-global-environment (setup-environment))
+
+(interpret '(define (require p) (if (not p) (amb))))
+
+(interpret '(define (distinct? items)
+              (cond ((null? items) true)
+                    ((null? (cdr items)) true)
+                    ((member (car items) (cdr items)) false)
+                    (else (distinct? (cdr items))))))
+
+(interpret '(define (multiple-dwelling)
+              (let ((baker (amb 1 2 3 4 5))
+                    (cooper (amb 1 2 3 4 5))
+                    (fletcher (amb 1 2 3 4 5))
+                    (miller (amb 1 2 3 4 5))
+                    (smith (amb 1 2 3 4 5)))
+                (require
+                 (distinct? (list baker cooper fletcher miller smith)))
+                (require (not (= baker 5)))
+                (require (not (= cooper 1)))
+                (require (not (= fletcher 5)))
+                (require (not (= fletcher 1)))
+                (require (> miller cooper))
+                ;(require (not (= (abs (- smith fletcher)) 1)))
+                (require (not (= (abs (- fletcher cooper)) 1)))
+                (list (list 'baker baker)
+                      (list 'cooper cooper)
+                      (list 'fletcher fletcher) (list 'miller miller)
+                      (list 'smith smith)))))
+
+(load "35_repl.scm")
