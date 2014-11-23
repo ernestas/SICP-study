d62ff585b3cfcc373492d4195c7d19efe9a356b2
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2014-11-23 13:34:00 +0000

    80

--- src/sicp/2/2/43.scm
+++ src/sicp/2/2/43.scm
@@ -8,7 +8,8 @@
 ;; Louis'
 
 (flatmap (lambda (new-row)
-           (map (lambda (rest-of-queens) (adjoin-position new-row k rest-of-queens))
+           (map (lambda (rest-of-queens)
+                  (adjoin-position new-row k rest-of-queens))
                 (queen-cols (- k 1))))
          (enumerate-interval 1 board-size))
 
